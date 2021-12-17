6325c4ec8be99ef5285dbf67412d20f21726efb2
Руслан Сайфетдинов <r.sayfetdinov@bars.group>
2021-12-17 17:06:15 +0300

    add .sln and Unit.Test project generation

---
+++ AspCore.Microservices.Template/AspCore.Microservices.Template.Test.Unit/UnitTest1.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace AspCore.Microservices.Template.Test.Unit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1() => Assert.Pass();
+    }
+}
